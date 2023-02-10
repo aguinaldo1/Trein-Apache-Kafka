ddfc7bfca07a0e85b41423db55f6ac960b978f1c
Aguinaldo Américo <aguinaldoamerico2@gmail.com>
2023-02-10 19:51:28 -0300

    subindo str-producer e arq docker

--- str-producer/src/main/java/com/valdir/strproducer/StrProducerApplication.java
+++ str-producer/src/main/java/com/valdir/strproducer/StrProducerApplication.java
@@ -1,4 +1,4 @@
-package com.example.strproducer;
+package com.valdir.strproducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StrProducerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StrProducerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StrProducerApplication.class, args);
+    }
 
 }
